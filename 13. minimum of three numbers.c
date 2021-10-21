3f1b764a42a9f082c654ab9046be66dc7f610d01
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-10-21 22:26:31 +0900

    fix : add code from the class

--- 13. minimum of three numbers.c
+++ 13. minimum of three numbers.c
@@ -5,13 +5,18 @@ int main() {
     scanf("%d %d %d", &a, &b, &c);
     int min;
 
-    if (a <= b && a <= c) {
-        min = a;
-    } else if (b <= c && b <= a) {
-        min = b;
-    } else {
-        min = c;
-    }
+//    if (a <= b && a <= c) {
+//        min = a;
+//    } else if (b <= c && b <= a) {
+//        min = b;
+//    } else {
+//        min = c;
+//    }
+
+    if (a<b) min = a;
+    else min = b;
+
+    if (c<min) min = c;
     printf("%d\n", min);
 
     return 0;
--- 14. determine if you can make a triangle.c
+++ 14. determine if you can make a triangle.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//
+//    if (a+b <= c || b+c <= a || c+a <= b) {
+//        printf("NO\n");
+//    } else {
+//        printf("YES\n");
+//    }
+
+    int a, b, c, max, total;
     scanf("%d %d %d", &a, &b, &c);
+    total = a+b+c;
     
-    if (a+b <= c || b+c <= a || c+a <= b) {
-        printf("NO\n");
-    } else {
-        printf("YES\n");
-    }
+    if (a > b) {
+        max = a;
+    } else max = b;
+    if (c > max) max = c;
+    
+    if ((total-max) > max) printf("YES\n");
+    else printf("NO\n");
     
     return 0;
 }
