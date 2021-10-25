34bfe2481bac1392023794bbfbfa201ff2c0305c
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-10-25 10:42:01 +0900

    fix : update part 20, 26

--- 20. determine if it's a prime number.c
+++ 20. determine if it's a prime number.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, i;
     scanf("%d", &n);
     
-    for (int i=2; i<=n-1; i++) {
+    for (i=2; i<n; i++) {
         if (n%i == 0) {
             printf("NO\n");
             break;
-        } else if (i == n-1) {
-            printf("YES\n");
-            break;
         }
     }
+    if (i==n) printf("YES\n");
     
     return 0;
 }
--- 26. find out the number of animals.c
+++ 26. find out the number of animals.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d", &theNumberOfAnimals, &theNumberOfLegs);
     
     int rabbit, chicken = 0;
-    for (rabbit = 1; rabbit <= theNumberOfAnimals; rabbit++) {
+    for (rabbit = 1; rabbit < theNumberOfAnimals; rabbit++) {
         chicken = theNumberOfAnimals - rabbit;
         if (rabbit * 4 + chicken * 2 == theNumberOfLegs) {
             break;
