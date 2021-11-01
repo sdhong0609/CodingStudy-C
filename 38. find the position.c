1db74d70f70daa3b38227728017adbf261efec4b
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-01 22:33:20 +0900

    fix : update part 38

--- 38. find the position.c
+++ 38. find the position.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-int arr[100];
+int arr[101];
 int main() {
-    int n, i, j, tmp;
+    int n, i, m;
     scanf("%d", &n);
-
     for (i=0; i<n; i++) {
         scanf("%d", &arr[i]);
     }
-    scanf("%d", &arr[n]);
-
-    for (i=0; i<n; i++) {
-        for (j=0; j<n-i; j++) {
-            if (arr[j] > arr[j+1]) {
-                tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
-            }
-        }
+    scanf("%d", &m);
+    for (i=n-1; i>=0; i--) {
+        if (arr[i]>m) arr[i+1] = arr[i];
+        else break;
     }
-    for (i=0; i<n+1; i++) {
+    arr[i+1] = m;
+    for (i=0; i<=n; i++) {
         printf("%d ", arr[i]);
     }
-
+    
     return 0;
 }
