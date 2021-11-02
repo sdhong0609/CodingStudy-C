9857ed6d1f2e37358a093ade375a72e795127862
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-02 22:09:19 +0900

    fix : update part 39

--- 39. the seven dwarfs.c
+++ 39. the seven dwarfs.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int arr[9];
-int ran[9];
-int arr2[9];
+int arr[10];
 int main() {
-    int i, j, k, sum;
+    int i, j, sum=0, tmp=0;
     for (i=0; i<9; i++) {
         scanf("%d", &arr[i]);
+        sum += arr[i];
     }
-    while (1) {
-        sum = 0;
-        for (i=0; i<9; i++) {
-            ran[i] = rand()%9;
-            for (j=0; j<i; j++) {
-                if (ran[i] == ran[j]) {
-                    i--;
-                    break;
-                }
+    for (i=0; i<8; i++) {
+        for (j=i+1; j<9; j++) {
+            tmp = sum - (arr[i]+arr[j]);
+            if (tmp == 100) {
+                arr[i]=-1;
+                arr[j]=-1;
+                break;
             }
         }
-
-        for (k=0; k<7; k++) {
-            arr2[k] = arr[ran[k]];
-            sum += arr[ran[k]];
-        }
-        if (sum == 100) break;
+        if (tmp == 100) break;
     }
-
-    for (i=0; i<7; i++) {
-        printf("%d ", arr2[i]);
+    for (i=0; i<9; i++) {
+        if (arr[i]>0) printf("%d ", arr[i]);
     }
-
+    
     return 0;
 }
