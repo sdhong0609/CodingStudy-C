58b81995ee192092f4a93c4e0a78bf17610860f2
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-03 22:38:18 +0900

    fix : update part 40

--- 40. cross the creek.c
+++ 40. cross the creek.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int arr[100];
+int arr[30];
 int main() {
-    int n;
+    int n, i;
     scanf("%d", &n);
     
     arr[0] = 1;
-    arr[1] = arr[0] + arr[0]; //2
-    for (int i=2; i<=n; i++) {
+    arr[1] = 1;
+    for (i=2; i<=n+1; i++) {
         arr[i] = arr[i-2] + arr[i-1];
     }
-    printf("%d\n", arr[n]);
+    printf("%d\n", arr[n+1]);
     
     return 0;
 }
