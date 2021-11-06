b25a15b4ad2bd112bdde9dac84d12f7f33b9100b
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-06 15:28:39 +0900

    feat : add part 43

--- 43. print out two-dimensional array 2.c
+++ 43. print out two-dimensional array 2.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
+int arr[11][11];
 int main() {
     int n, i, j, k=1;
     scanf("%d", &n);
-    int arr[n][n];
-    
-    for (i=0; i<n; i++) {
-        for (j=0; j<n; j++) {
-            arr[i][j] = 0;
-        }
-    }
-    
     for (i=0; i<n; i++) {
         for (j=i; j>=0; j--) {
             arr[i][j] = k;
