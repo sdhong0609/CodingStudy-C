7c6f4f7a6ec07703e1961d96e745269411054e5c
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-08 21:32:27 +0900

    feat : add part 48

---
+++ 48. find prime number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n) {
+    if (n==1) return false;
+    for (int i=2; i<n; i++) {
+        if (n%i==0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int n, a;
+    scanf("%d", &n);
+    for (int i=0; i<n; i++) {
+        scanf("%d", &a);
+        if (isPrime(a)) printf("%d ", a);
+    }
+    return 0;
+}
